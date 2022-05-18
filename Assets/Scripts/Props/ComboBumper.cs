985fedf271c185fc183b9475a2656d8995548370
chilimane <87211812+jkleland@users.noreply.github.com>
2022-05-17 19:57:59 -0700

    ComboBumper script

---
+++ Assets/Scripts/Props/ComboBumper.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class ComboBumper : MonoBehaviour
+{
+    public Transform[] next;
+
+    public float force = 15f;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "WindAffectable")
+        {
+            int randomNext = Random.Range(0, next.Length);
+            Vector3 nextPos = next[randomNext].position;
+            Vector3 direction = nextPos - collision.transform.position;
+
+            Rigidbody ballRb = collision.collider.GetComponent<Rigidbody>();
+            ballRb.velocity = Vector3.zero;
+            ballRb.angularVelocity = Vector3.zero;
+            ballRb.AddForce(direction.normalized * force, ForceMode.Impulse);
+        }
+    }
+}
