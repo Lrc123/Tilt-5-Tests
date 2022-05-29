d8b8c5d7f1807a4a9000f51ed97f95c504820471
jkleland <jkleland@ucsc.edu>
2022-05-28 20:10:12 -0700

    guide

---
+++ Assets/MonsterPop.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterPop : MonoBehaviour
+{
+    public Animator MPop;
+    private bool isEnd = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isEnd)
+            return;
+        //capDropped();
+    }
+
+    private void capDropped()
+    {
+        if(transform.position.y < 0.5f)
+        {
+            MPop.SetTrigger("Pop");
+            Destroy(MPop.gameObject, 2.8f);
+            isEnd = true;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag.Equals("Ground"))
+        {
+            MPop.SetTrigger("Pop");
+            Destroy(MPop.gameObject, 2.8f);
+            isEnd = true;
+        }
+    }
+}
